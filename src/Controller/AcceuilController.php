51b01d55af6889697d0d10d820bde4f44c07b90b
Mikael LESGUER <mikedev35@outlook.fr>
2023-08-01 17:04:32 +0200

    mise en place 2

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -2,20 +2,35 @@
 
 namespace App\Controller;
 
+use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AcceuilController extends AbstractController
 {
-    /**
-     * @Route('/acceuil', name: 'app_acceuil')]
-     */
-    #[Route('/acceuil', name: 'app_acceuil')]
+
+    #[Route('/about', name: 'A-PROPOS')]
     public function index(): Response
     {
         return $this->render('acceuil/index.html.twig', [
             'controller_name' => 'AcceuilController',
         ]);
+    }
+
+    #[Route('/', name: 'home-page')]
+    public function home()
+    {
+        return $this->render('acceuil/home.html.twig');
+//            , [
+//            'title' => "Bienvenue a vous sur mon portfolio",
+//            'age' => 31
+//        ]);
+    }
+
+    #[Route('/about/12', name: 'blog_show')]
+    public function show()
+    {
+        return $this->render('acceuil/show.html.twig');
     }
 }
